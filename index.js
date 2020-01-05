642ecbc911dccb01123220fddb62dd9aaba853a2
Richell Bonfield <richell.bonfield12@gmail.com>
2020-01-05 21:38:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
 
-const append;
-const prepend;
-const removeFirst;
-const removeLast;
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
